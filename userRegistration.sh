241d773e9f8afc3e95067096bd96c09b550f1a61
tushar8871 <tusharkhachane6@gmail.com>
2020-03-25 19:36:36 +0530

    Added : password must have at least 1 numeric character

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,7 +4,7 @@
 NAMEPATTERN="^[A-Z][a-z]{3,}$"
 EMAILPATTERN="^[a-zA-Z0-9._a-zA-Z0-9]+@[a-zA-Z0-9.a-zA-Z0-9]+\.[A-Za-z]{2,4}$"
 MOBILEPATTERN="^[0-9]{2} [7-9]{1}[0-9]{9}$"
-PASSWORDPATTERN="^[a-zA-Z0-9]*(.*[A-Z].*{1}+)*[a-zA-Z0-9]{7,}$"
+PASSWORDPATTERN="^[a-zA-Z0-9]*(([0-9][A-Z]+[a-zA-Z0-9]){2}|([A-Z][0-9]+[a-zA-Z0-9]*){2})*[A-Za-z0-9]*{6,}$"
 
 #variables
 firstName=""
@@ -19,25 +19,25 @@ echo "Welcome to user registration validation ! "
 read -p "Enter first name : " firstName
 if [[ $firstName =~ $NAMEPATTERN ]]
 then
-	echo "Valid Name"
+	echo "first name is valid"
 else
-	echo "Not valid name"
+	echo "Please enter first letter as capital"
 fi
 
 #read last name
 read -p "Enter Last name : " lastName
 if [[ $lastName =~ $NAMEPATTERN ]]
 then
-	echo "Valid Last name"
+	echo "Last name is valid"
 else
-	echo "Not valid Last name"
+	echo "Please enter first letter as capital"
 fi
 
 #read email
 read -p "Enter email : " email
 if [[ $email =~ $EMAILPATTERN ]]
 then
-	echo "Valid email"
+	echo "Email id is valid"
 else
 	echo "Not valid email"
 fi
