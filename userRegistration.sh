bb27e4c7e09a8dc76ef5db3583666917d496f544
tushar8871 <tusharkhachane6@gmail.com>
2020-03-25 15:40:32 +0530

    Added : check email is valid or not

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,29 +1,40 @@
 #!/bin/bash -x
 
 #constants
-namePattern="^[A-Z][a-z]{3,}$"
+NAMEPATTERN="^[A-Z][a-z]{3,}$"
+EMAILPATTERN="^[a-zA-Z0-9._a-zA-Z0-9]+@[a-zA-Z0-9.a-zA-Z0-9]+\.[A-Za-z]{2,4}$"
 
 #variables
 firstName=""
 lastName=""
+email=""
 
 #welcome message
 echo "Welcome to user registration validation ! "
 
-#read name from user
+#read first name from user
 read -p "Enter first name : " firstName
-if [[ $firstName =~ $namePattern ]]
+if [[ $firstName =~ $NAMEPATTERN ]]
 then
 	echo "Valid Name"
 else
 	echo "Not valid name"
 fi
 
+#read last name
 read -p "Enter Last name : " lastName
-
-if [[ $lastName =~ $namePattern ]]
+if [[ $lastName =~ $NAMEPATTERN ]]
 then
 	echo "Valid Last name"
 else
 	echo "Not valid Last name"
+fi
+
+#read email
+read -p "Enter email : " email
+if [[ $email =~ $EMAILPATTERN ]]
+then
+	echo "Valid email"
+else
+	echo "Not valid email"
 fi
