dc7725ad30030443827d596d328967c751d2d9e8
tushar8871 <tusharkhachane6@gmail.com>
2020-03-25 14:25:51 +0530

    Added : last name start with capital letter and have min 3 char

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,16 +5,25 @@ namePattern="^[A-Z][a-z]{3,}$"
 
 #variables
 firstName=""
+lastName=""
 
 #welcome message
 echo "Welcome to user registration validation ! "
 
 #read name from user
 read -p "Enter first name : " firstName
-
 if [[ $firstName =~ $namePattern ]]
 then
 	echo "Valid Name"
 else
 	echo "Not valid name"
+fi
+
+read -p "Enter Last name : " lastName
+
+if [[ $lastName =~ $namePattern ]]
+then
+	echo "Valid Last name"
+else
+	echo "Not valid Last name"
 fi
