00fa379a82ccf58d554f1aeec6841b97ab6d6da2
tushar8871 <tusharkhachane6@gmail.com>
2020-03-25 14:13:17 +0530

    Added : first letter of first name be capital and have minimum 3 char

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 
+#constants
+namePattern="^[A-Z][a-z]{3,}$"
+
+#variables
+firstName=""
+
+#welcome message
 echo "Welcome to user registration validation ! "
+
+#read name from user
+read -p "Enter first name : " firstName
+
+if [[ $firstName =~ $namePattern ]]
+then
+	echo "Valid Name"
+else
+	echo "Not valid name"
+fi
